ea587c6f6af0fc11bd6ce63f14f8b9ec0cb392e8
Andrezj <33471054+Andrezj@users.noreply.github.com>
2018-09-19 11:05:05 -0300

    Update main.cpp

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+//Esboço de Função de Busca.
+
+string;
+string pesquisaDisc(char entrada[]){
+    string arrayDeDisciplinas[] = {""}; //Atribuir á variável Global aqui.
+    string saida = "";
+    for(int i = 0; i < arrayDeDisciplinas.size(); i++){
+        int j = 0;
+        bool add = true;
+        char caracter = entrada[0];
+        while(caracter != '/n'){
+            if(caracter != arrayDeDisciplinas[i][j]){
+                add = false;
+                break;
+            }
+            j++;
+            caracter = entrada[j];
+        }
+        if(add){
+            saida += arrayDeDisciplinas[i] + "/n";
+        }
+    }
+    if(saida == ""){
+        saida = "Não foram encontradas disciplinas que corresopondam á pesquisa.";
+    }
+    return saida;
+}
+
+//Fim da Função
 
 int main() {
     return 0;
