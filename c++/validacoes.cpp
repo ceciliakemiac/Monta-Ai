1aed75013fda3da6bceb0725c2e9622c0dc6f340
izaquielnn <izaquiel.nascimento@ccc.ufcg.edu.br>
2018-09-20 01:07:37 -0300

    Adicionadas validações de choque de horario e quantidade de créditos

---
+++ c++/validacoes.cpp
@@ -0,0 +1,32 @@
+int deuChoqueDeHorarios(turma t1, turma t2){
+    int choque = 0;
+    for(horario ht1 : t1.horarios){
+        for(horario ht2 : t2.horarios){
+            if(ht1.dia == ht2.dia && ht1.hora == ht2.hora){
+                choque = 1;
+                break;
+            }
+        }
+    }
+    return choque;
+}
+
+int totalDeCreditos(vector<disciplina> disciplinas){
+    int total = 0;
+    for(disciplina d : disciplinas){
+        total += d.creditos;
+    }
+    return total;
+}
+
+// retorna -1 se estiver abaixo do mínimo de créditos exigido(15),
+// 1 se estiver acima do máximo(24) e 0 se estiver tudo certo
+int validaQtdCreditos(int totalDeCreditos){
+    int valida = 0;
+    if(totalDeCreditos < 15){
+        valida = -1;
+    } else if(totalDeCreditos > 24){
+        valida = 1;
+    }
+    return valida;
+}
