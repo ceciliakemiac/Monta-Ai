cc0128eb3bf4ff988337afefc82da9bcbc098b72
ceciliakemiac <ceciliakemiac2@gmail.com>
2018-09-20 00:11:17 -0300

    ajeitar tab

--- c++/menu.cpp
+++ c++/menu.cpp
@@ -4,10 +4,10 @@ using namespace std;
 void rodaMenu() {
     cout << "MENU" << "\n";
     cout << "1 - Montar Horário do semestre" << "\n";
-	cout << "2 - Visualizar disciplinas" << "\n";
-	cout << "3 - Ver informações detalhadas de uma disciplina" << "\n";
-	cout << "4 - Avaliar disciplina" << "\n";
-    cout << "5 - SAIR" << "\n"
+    cout << "2 - Visualizar disciplinas" << "\n";
+    cout << "3 - Ver informações detalhadas de uma disciplina" << "\n";
+    cout << "4 - Avaliar disciplina" << "\n";
+    cout << "5 - SAIR" << "\n";
 }
 
 int main() {
