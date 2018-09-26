f19c6470ee793c7a145a0efc5af471be52407681
izaquielnn <izaquiel.nascimento@ccc.ufcg.edu.br>
2018-09-25 22:03:06 -0300

    adicionada função para visualização de um quadro

--- c++/funcoesAuxiliares.cpp
+++ c++/funcoesAuxiliares.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include "disciplinas_bd.cpp"
 using namespace std;
 
 void rodaMenu() {
@@ -11,7 +12,69 @@ void rodaMenu() {
     cout << "5 - SAIR" << "\n";
 }
 
-void printHorario(vector<disciplina> disciplinas) {
+void printHorario(vector<string> h[5][5], int marcarChoques){
+
+	string BOLD = "\x1B[1m";
+	string RED = "\x1B[31m";
+	string GREEN = "\x1B[32m";
+	string RESTORE_COLOR = "\x1B[0m";
+
+	if(!marcarChoques){
+		RED = RESTORE_COLOR;
+		GREEN = RESTORE_COLOR;
+	}
+
+	int w = 15;
+	cout << "        ";
+	cout << left << setw(w) << setfill(' ') << "SEGUNDA";
+	cout << left << setw(w) << setfill(' ') << "TERÇA";
+	cout << left << setw(w) << setfill(' ') << "QUARTA";
+	cout << left << setw(w) << setfill(' ') << "QUINTA";
+	cout << left << setw(w) << setfill(' ') << "SEXTA";
+	cout << endl;
+
+	int numMaxDisciplina;
+	for(int hora = 0; hora < 5; hora++){
+
+		// 2 representa 12 horas (2 * 2 + 8), logo não tem disciplinas nesse horario
+		if(hora == 2) {
+			cout << endl;
+			continue;
+		}
+
+		// Pega a hora(comparando em cada dia da semana) com maior quantidade de disciplinas para ajustar o tamanho da tabela
+		numMaxDisciplina = 0;
+		for (int dia = 0; dia < 5; dia++){
+			if(h[dia][hora].size() > numMaxDisciplina) numMaxDisciplina = h[dia][hora].size();
+		}
+
+
+		for(int idisc = 0; idisc < numMaxDisciplina; idisc++){
+
+			if(idisc == 0) cout << left << setw(7) << setfill(' ')  << to_string((hora * 2) + 8) + ":00"; 
+			else cout << "       ";
+
+			for(int dia = 0; dia < 5; dia++){
+
+				if( idisc < h[dia][hora].size()){
+
+					if(h[dia][hora].size() > 1) {
+						cout << BOLD << RED << left << setw(w) << setfill(' ') <<  h[dia][hora][idisc] << RESTORE_COLOR;
+					} else {
+						cout << BOLD << GREEN <<  left << setw(w) << setfill(' ') <<  h[dia][hora][idisc] << RESTORE_COLOR;                                           
+					}
+					
+				} else cout << left << setw(w) << setfill(' ') << "";
+			}
+			cout << endl;
+		}
+		cout << endl;
+
+	}
+
+}
+
+void printDisciplinas(vector<disciplina> disciplinas) {
 
 	vector<string> h[5][5];
 	for(disciplina d: disciplinas){
@@ -22,36 +85,23 @@ void printHorario(vector<disciplina> disciplinas) {
 		}
 	}
 	
-
-	int w = 30;
-	cout << left << setw(w) << setfill(' ') << "SEGUNDA";
-	cout << left << setw(w) << setfill(' ') << "TERÇA";
-	cout << left << setw(w) << setfill(' ') << "QUARTA";
-	cout << left << setw(w) << setfill(' ') << "QUINTA";
-	cout << left << setw(w) << setfill(' ') << "SEXTA";
-	cout << endl;
-
-	int maior;
-	for(int i = 0; i < 5; i++){
-		cout << left << setw(w * 5) << setfill('-') << to_string((i * 2) + 8) + ":00" << endl;
-		if(i == 2) {
-			continue;
-		}
-		maior = 0;
-		for (int l = 0; l < 5; l++){
-			if(h[l][i].size() > maior) maior = h[l][i].size();
-		}
-		for(int j = 0; j < maior; j++){
-			for(int k = 0; k < 5; k++){
-				if( j < h[k][i].size()){
-					cout << left << setw(w) << setfill(' ') <<  h[k][i][j];                                           
-				} else cout << left << setw(w) << setfill(' ') << "";
-			}
-			cout << endl;
-		}
-
-	}
+	printHorario(h, 0);
 }
+
+
+
+void printQuadro(quadro q){
+	vector<string> h[5][5];
+	for(celula cel: q.celulas){
+		for(horario hr: cel.turmaDisciplina.horarios){
+			h[hr.dia - 2][(hr.hora - 8) / 2].push_back(cel.nomeDisciplina);
+		}
+	}
+
+	printHorario(h, 1);
+}
+
+
 
 // Metodos de iteracao para visualizacao das disciplinas
 void visualizarDisciplinaDet(){
