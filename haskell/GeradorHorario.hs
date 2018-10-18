c449f916e9bcfea16aaec851715daab1c4cbfff6
izaquielnn <izaquiel.nascimento@ccc.ufcg.edu.br>
2018-10-18 19:23:12 -0300

    add função que gera todas combinações de disciplinas para montagem dos horarios

--- haskell/GeradorHorario.hs
+++ haskell/GeradorHorario.hs
@@ -10,3 +10,17 @@ filtrarDiaeHora d h disciplinas = [x | x <- disciplinas, horarioBate d h (horari
 
 geraTabelaHorario :: [Disciplina_matricula] -> [[[Disciplina_matricula]]]
 geraTabelaHorario disciplinas = [[ filtrarDiaeHora dia hora disciplinas | hora <- [8, 10..16]] | dia <- [2..6]]
+
+gerarPossiveisHorarios :: [Disciplina_matricula] -> [[Disciplina_matricula]]
+gerarPossiveisHorarios disc = [ gera disc b | b <- comBinario ]
+            where
+                nBits = length disc
+                maxComb = 2 ^ nBits - 1
+                comBinario = [decimalTobinario n nBits | n <- [1..maxComb]] 
+
+
+gera :: [Disciplina_matricula] -> [Int] -> [Disciplina_matricula]
+gera [] [] = []
+gera (d:ds) (b:bs)
+            | b == 1 = [d] ++ gera ds bs
+            | otherwise = gera ds bs
