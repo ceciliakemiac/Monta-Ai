f20729b0c1e7b29e93a534ed70ac5c6e84d59223
jardelymaris <jardely.santos@ccc.ufcg.edu.br>
2018-10-21 23:01:56 +0800

    Avaliacao em txt - Obg Deus

--- haskell/Avaliacao.hs
+++ haskell/Avaliacao.hs
@@ -1,8 +1,12 @@
-module Avaliacao
-(passoAvaliacao) where
+module Avaliacao where
 
 import BancoDisciplinas
 import Estruturas
+import Data.List.Split
+import System.IO  
+import System.Directory  
+import Data.List  
+
 
 comentario_ou_votarAvaliacao = do
     putStrLn "1 - Deixar um comentário"
@@ -51,5 +55,61 @@ addComentario aval comentario = Avaliacao { nomeDisciplina = n, comentarios = co
         niv = (nivel aval)
         com = ((comentarios aval) ++ [comentario])
 
+adicionarComentario :: IO ()
+adicionarComentario = do
+    putStrLn ("Digite o nome da disciplina: ")
+    nome <- getLine
+    putStrLn ("Digite o comentario: ")
+    comentario <- getLine
+    avaliacoes <- pegarAvaliacao
+    atualizaTxt nome avaliacoes
+    appendFile "avaliacao.txt" (nome ++ ";" ++ (pegaNivel nome avaliacoes ) ++ ";" ++ comentario ++ " " ++
+                (pegaComentarios nome avaliacoes) ++ "\n") 
+
+atribuirNivel :: IO ()
+atribuirNivel = do
+    putStrLn ("Digite o nome da disciplina: ")
+    nome <- getLine
+    putStrLn ("Digite o nivel: ")
+    nivel <- getLine
+    avaliacoes <- pegarAvaliacao
+    atualizaTxt nome avaliacoes
+    appendFile "avaliacao.txt" (nome ++ ";" ++ nivel ++ ";" ++
+                (pegaComentarios nome avaliacoes) ++ "\n") 
+
+atualizaTxt :: String -> [String] -> IO ()
+atualizaTxt nome avaliacoes = do
+    handle <- openFile "avaliacao.txt" ReadMode  
+    writeFile "novo.txt" ""
+    contents <- hGetContents handle  
+    let todoTasks = lines contents   
+    let newTodoItems = deletar nome avaliacoes todoTasks
+    appendFile "novo.txt" (unlines newTodoItems) 
+    hClose handle  
+    removeFile "avaliacao.txt"  
+    renameFile "novo.txt" "avaliacao.txt"  
+
+deletar :: String -> [String] -> [String] -> [String]
+deletar nome avaliacoes array =  delete (array !! (pegaIndice nome avaliacoes 0)) array
+
+pegaIndice :: String -> [String] -> Int -> Int
+pegaIndice nome [] n = n - (n+1)
+pegaIndice nome (a:as) n = if ((splitOn ";" a) !! 0) == nome then n
+                            else 0 + pegaIndice nome as (n + 1)
+
+pegaNivel :: String -> [String] -> String
+pegaNivel nome aval = (pegarDadosAval aval nome) !! 1
+
+pegarDadosAval:: [String] -> String -> [String]
+pegarDadosAval avaliacoes nome =  splitOn (";") (unwords [aval | aval <- avaliacoes, nome == ((splitOn ";" aval) !! 0)]) 
 
 
+pegarAvaliacao :: IO [String]
+pegarAvaliacao = do
+    contents <- readFile "avaliacao.txt"   
+    let todoTasks = lines contents
+    return (zipWith (\n line -> line) [0..] todoTasks)
+
+pegaComentarios :: String -> [String] -> String
+pegaComentarios nome aval = if ((pegarDadosAval aval nome) !! 2) == "*" then ""
+                            else (pegarDadosAval aval nome) !! 2
