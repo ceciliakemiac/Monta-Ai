0e9d1dfb0cc2e60b7337635c9cb1a655dbca60fd
ceciliakemiac <ceciliakemiac2@gmail.com>
2018-10-23 17:43:47 -0300

    Planilha com os horários

--- haskell/principal.hs
+++ haskell/principal.hs
@@ -2,6 +2,7 @@ import Estruturas
 import BancoDisciplinas
 import Menu
 import GeradorHorario
+import Planilhas
 
 
 main :: IO ()
@@ -9,6 +10,21 @@ main = do
 
     Menu.logo
     Menu.principal
+    -- putStrLn (show (geraTabelaHorario [a,b,c]))
+    -- putStrLn (cabecalhoTabelaHorario)
+    -- putStrLn (toString fmcc1)
+    -- putStrLn (show (dia ((horarios ((turmas fmcc1) !! 0)) !! 0)))
+
+    -- putStrLn (show ((turmas fmcc1) !! 0))
+    -- putStrLn (toStringDisciplinaTurma fmcc1 "1")
+
+    -- putStrLn (show (getHorarios (turmas fmcc1)))
+    -- putStrLn (show (getHorarios (turmas fut)))
+    putStrLn (string)
+
+    -- putStrLn (show (geraTabelaHorario [a,b,c]))
+    -- putStrLn (show (printHorario [a,b,c]))
+
     -- print "Teste de pre-requisitos"
     -- print ((atendePreRequisitos (getDisciplinaNome "P1") []) == True)
     -- print ((atendePreRequisitos (getDisciplinaNome "P2") ["P1", "LP1"]) == True)
