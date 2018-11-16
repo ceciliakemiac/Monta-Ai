8720f39945d4d916e6632e9cca1c98192a25a18f
ceciliakemiac <ceciliakemiac2@gmail.com>
2018-11-15 23:00:17 -0300

    dá erro se disciplinas dos primeiros periodos forem adicionadas antes ????

--- prolog/horariosPagos.pl
+++ prolog/horariosPagos.pl
@@ -5,45 +5,35 @@
 
 existeDisciplina(Nome):- turmas:disciplina(Nome, _, _, _, _, _).
 
-digitaDisciplinas():-
+digitaDisciplinas:-
     read_line_to_string(user_input, Nome),
-    (Nome = "pronto" -> halt(0);
-     existeDisciplina(Nome) -> assert(disciplinaPaga(Nome));
-     writeln("ERRO! Não existe essa daí."), digitaDisciplinas()).
+    (Nome = "pronto" -> false;
+     existeDisciplina(Nome) -> assert(disciplinaPaga(Nome)), digitaDisciplinas;
+     writeln("ERRO! Não existe essa daí."), digitaDisciplinas).
 
-adicionaDisciplinaPaga():-
-    forall(disciplinaPaga(X)),
-    turmas:disciplina(X, _, _, _, Y, _),
-    adicionaPreRequisito(Y).
+getPreRequisito(X, Y):- 
+    (turmas:disciplina(X, _, _, _, Y, _) -> true;
+     true).
     
 adicionaPreRequisito([S]):- 
-    not(disciplinaPaga(S)) -> assert(disciplinaPaga(S))).
+    (not(disciplinaPaga(S)) -> assert(disciplinaPaga(S)), true;
+     true).
 adicionaPreRequisito([H | T]):-
     (not(disciplinaPaga(H)) -> assert(disciplinaPaga(H))),
     adicionaPreRequisito(T).
 
-% digitaDisciplinas(Discs):-
-%     read_line_to_string(user_input, Nome),
-%     (Nome = "pronto" -> halt(0);
-%      existeDisciplina(Nome) -> append([Nome], Discs, D), digitaDisciplinas(D);
-%      writeln("ERRO! Não existe essa daí."), digitaDisciplinas(Discs)).
-        
-% digitaDisciplinas(D):-
-%     read_line_to_string(user_input, Nome),
-%     Nome = "pronto",
-%     D = [].
+iteraPagasAdicionaRequisitos([]).
+iteraPagasAdicionaRequisitos([H | T]):-
+    getPreRequisito(H, Y),
+    adicionaPreRequisito(Y),
+    iteraPagasAdicionaRequisitos(T).
 
-% disciplinasPagas("pronto", []).
-% disciplinasPagas(Nome, [X | T]):-
-%     existeDisciplina(Nome),
-%     read_line_to_string(user_input, Novo_nome),
-%     disciplinasPagas(Novo_nome, [Nome, X | T]).
+iteraResumido:-
+    findall(Y, disciplinaPaga(Y), X),
+    iteraPagasAdicionaRequisitos(X).
 
-% :- initialization(main).
 
-% main:-
-%     writeln("Digite as disciplinas que já pagou: "),
-%     read_line_to_string(user_input, Disc),
+
 
 
 
