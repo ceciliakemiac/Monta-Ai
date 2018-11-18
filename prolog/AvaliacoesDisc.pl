4a1794c912f3b03c0847f1c31b28eaecca7ecba8
jardelymaris <jardely.santos@ccc.ufcg.edu.br>
2018-11-19 07:42:12 +0800

    Uma hora vai.

--- prolog/AvaliacoesDisc.pl
+++ prolog/AvaliacoesDisc.pl
@@ -1,10 +1,12 @@
 :- initialization(main).
 
 comentario_ou_votarAvaliacao :- 
+    writeln(""),    
     writeln("1 - Deixar um comentário"),
     writeln("2 - Votar em uma avaliação"). 
 
 menu_votar_avaliacao :- 
+    writeln(""),
     writeln("O que achou da disciplina?"),
     writeln("1 - rasgada"),
     writeln("2 - de boa"),
@@ -18,15 +20,46 @@ nivel(3, carrego).
 nivel(4, tenso).
 nivel(5, peso).
 
+passoAvaliacao :-
+    writeln(""),
+    writeln("Qual disciplina deseja avaliar?"),
+    read_line_to_string(user_input, Disc),
+    (avaliacao(Disc) -> escolherOpcao(Disc); 
+    writeln("Disciplina não existe /:")),
+    writeln("1 - Avaliar outra disciplina"), 
+    writeln("2 - voltar ao menu"),
+    read_line_to_string(user_input, Opcao),
+    (Opcao = "1" -> passoAvaliacao;
+    Opcao = "2" -> main).
+
+escolherOpcao(Disc) :- 
+    comentario_ou_votarAvaliacao,
+    read_line_to_string(user_input, Opcao),
+    (Opcao = "2" -> menu_votar_avaliacao, atribuirNivel(Disc);
+    adicionarComentario(Disc)).
+
+atribuirNivel(Disc) :- avaliacao(Disc), writeln("Nivel a implementar").
+adicionarComentario(Disc) :- avaliacao(Disc), writeln("Cmen a implementar").
+
+
+% adicionaAval([]).
+% adicionaAval([H|T]) :- 
+
+avaliacao("p1").
+    
+     
+    
+    
 
 main :- 
-   avaliacao(p1, X, _), insere(2, X, Res), writeln(Res).
- 
-# insere(Elem, Lista, [Elem|Lista]).
+    writeln("voltou"),
+    passoAvaliacao.
+%    avaliacoes(p1, X, _), insere(2, X, Res), writeln(Res).
+%    insere(Elem, Lista, [Elem|Lista]).
 
 
-# getAvalNome(Nome, Res) :- avaliacoes([Nome|Res]).
+% getAvalNome(Nome, Res) :- avaliacoes([Nome|Res]).
 
-# avaliacao(p1, [], []).
+avaliacoes(p1, [], []).
 
-# alteraNivel(Nome, Novo) :- avaliacoes(Nome, X, _), append(X, Novo, Res), writeln(Res).
+alteraNivel(Nome, Novo) :- avaliacoes(Nome, X, _), append(X, Novo, Res), writeln(Res).
